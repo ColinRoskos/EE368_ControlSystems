1f3dc26949a4da0cbdf0620183cb2056d5eef488
Colin Roskos <cfroskos@gmail.com>
2023-03-23 15:52:43 -0500

    lab5 complete

--- Lab5/Lab5.m
+++ Lab5/Lab5.m
@@ -42,5 +42,65 @@ title("System b");
 
 %% Exercise 2
 % Spring-Mass System
+% M = 1;
+% B = 1;
+% K = 1;
+% 
+% T_2 = tf([1], [M B K]);
+% wn = sqrt(K/M);
+% ze = B/(2*M*wn);
 
+ze_table = [.1 .4 .7 1. 2.];
+wn = 1;
+leg = []
+
+t = 0 : .01 : 12;
+
+for i=1:5
+    ze = ze_table(i);
+
+    T = tf([wn^2], [1 2*ze*wn wn^2])
+    step(T); hold on;
+    leg = [leg strcat("zeta = ", string(ze))];
+    S1 = stepinfo(T, "SettlingTimeThreshold", .02);
+    
+    
+    S2 = stepinfo(T, 'RiseTimeThreshold',[0.1 0.9]);
+
+    fprintf("\nRise Time (Z = %.1f) : %6.4f\n", ze, S2.RiseTime);
+    fprintf("Peak Time (Z = %.1f) : %6.4f\n", ze, S2.PeakTime);
+    fprintf("Overshoot (Z = %.1f) : %3.1f%%\n", ze, S2.Overshoot);
+    fprintf("Settling Time (Z = %.1f) : %6.4f\n", ze, S2.SettlingTime);
+    
+    SteadyState = tzw_response(1000, ze, wn);
+    SteadyState = SteadyState(end);
+    fprintf("Steady State (Z = %.1f) : %6.4f\n", ze, SteadyState);
+
+    settling_time = 4/(ze * wn);
+    fprintf("Calculated Settling Time (Z = %.1f) : %6.4f\n\n", ze, settling_time);
+    
+end
+legend(leg);
+title("Step Response with varying dampenning zeta")
+%%
+Ts = 4/(ze*wn)
+
+T_w2 = tf([wn^2], [1 2*ze*wn wn^2]);
+
+subplot(2,1,1);
+step(T_2);
+subplot(2,1,2);
+[t_y, y] = step(T_w2, 15);
+plot(y, t_y);
+max_value = max(t_y)
+max_value_t = t_y(Ts)
+
+function [t, y] = zw_response(ze, wn)
+    T = tf([wn^2], [1 2*ze*wn wn^2]);
+    [t, y] = step(T);
+end
+
+function y = tzw_response(t, ze, wn)
+    y = 1 - 1/(sqrt(1-ze)).*exp(-1*ze*wn.*t).*sin(wn*sqrt(1-ze^2.*t)+acos(ze));
+end
 
